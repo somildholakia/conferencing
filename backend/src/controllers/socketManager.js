5ad69b42677cab19895cbc7069b3737c695405a4
somildholakia <somildholakia0852@gmail.com>
2026-03-29 19:23:23 +0530

    update

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -1,10 +1,39 @@
-import { Server } from "socket.io"
+import { Server, socket } from "socket.io"
+
+let connections = {}
+let messages = {}
+let timeOnline= {}
 
 
 
 
 const connectToSocket  = (server) => {
     const io = new Server(server);
+
+    io.on("connection",(socket) => {
+
+        socket.on("join-call",(path) => {
+
+            if(connections[path] === undefined){
+                connections[path] = []
+            }
+            connections[path].push(socket.id)
+
+        })
+
+        socket.on("signal",(toId,message) => {
+            io.to(toId).emit("signal",socket.id,message);
+        })
+
+        socket.on("chat-message",(data,sender) => {
+
+        })
+
+        socket.on("disconnect",() => {
+
+        })
+
+    })
 
     return io;
 }
